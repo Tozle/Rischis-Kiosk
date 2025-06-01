2445f15fb51e76a70ed90a6e13245617f526f219
Tozle <t.zeiler@outlook.de>
2025-06-01 19:42:47 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  'https://vlmxcyputvvxfccegyae.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXhjeXB1dHZ2eGZjY2VneWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODgzMjMsImV4cCI6MjA2NDM2NDMyM30.LOVS3woAmgmrHedQzBkinsSh3gbPSJkCZMoeSS8ilSs'
 );
+
