5fec8e2b549e74ec28f7fed6a8e2d1ed7ff1dc24
Tozle <t.zeiler@outlook.de>
2025-06-01 18:58:26 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vlmxcyputvvxfccegyae.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXhjeXB1dHZ2eGZjY2VneWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODgzMjMsImV4cCI6MjA2NDM2NDMyM30.LOVS3woAmgmrHedQzBkinsSh3gbPSJkCZMoeSS8ilSs';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://vlmxcyputvvxfccegyae.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXhjeXB1dHZ2eGZjY2VneWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODgzMjMsImV4cCI6MjA2NDM2NDMyM30.LOVS3woAmgmrHedQzBkinsSh3gbPSJkCZMoeSS8ilSs'
+);
