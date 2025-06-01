daae9f79f6972bdc02b699a8b0de9982288296a6
Tozle <t.zeiler@outlook.de>
2025-06-01 19:20:51 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://vlmxcyputvvxfccegyae.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXhjeXB1dHZ2eGZjY2VneWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODgzMjMsImV4cCI6MjA2NDM2NDMyM30.LOVS3woAmgmrHedQzBkinsSh3gbPSJkCZMoeSS8ilSs";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
